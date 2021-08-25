f14bdf1f778b35357cabfc7278b51e5991146a28
Manish Singh <manish.uit.bu@gmail.com>
2021-08-25 19:29:51 -0400

    added the explicit_wait_2() method, it takes the polling duration as arguement

--- seleniumBasics/wait_explicit_wait.py
+++ seleniumBasics/wait_explicit_wait.py
@@ -5,18 +5,27 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from seleniumBasics.browser_invoking import invoke_browser
 
+TIMEOUT = 10
+Polling_Duration = 5
+url = "https://www.google.com/"
+driver = invoke_browser()
+driver.get(url)
 
 def explicit_wait():
-    TIMEOUT = 10
-    url = "https://www.google.com/"
-    driver = invoke_browser()
-    driver.get(url)
+
     wait = WebDriverWait(driver, TIMEOUT)
     search_box = wait.until(EC.visibility_of_element_located((By.NAME, 'q')))
     search_box.send_keys("explicit wait", Keys.RETURN)
     driver.close()
 
 
+def explicit_wait_2():
+    wait = WebDriverWait(driver, TIMEOUT, Polling_Duration, ignored_exceptions=None)
+    search_box = wait.until(EC.visibility_of_element_located((By.NAME, 'q')))
+    search_box.send_keys("explicit wait", Keys.RETURN)
+    driver.close()
+
+
 if __name__ == '__main__':
-    explicit_wait()
+    explicit_wait_2()
 
