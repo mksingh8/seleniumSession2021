39ae9cc9975a0bfc1a4ac9ff8b3477217bd763d0
Manish Singh <manish.uit.bu@gmail.com>
2021-08-25 18:48:17 -0400

    implicit wait code added

---
+++ seleniumBasics/implicitWait.py
@@ -0,0 +1,13 @@
+from seleniumBasics.browser_invoking import invoke_browser
+
+
+def implicit_wait():
+    TIMEOUT = 10
+    url = "https://www.google.com/"
+    driver = invoke_browser()
+    # this wait let the webdriver wait for TIMEOUT second for the DOM to load in case intended element is not loaded
+    # and then throws "No Such Element Exception" error
+    driver.implicitly_wait(TIMEOUT)
+    driver.get(url)
+    driver.close()
+
